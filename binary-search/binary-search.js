4d2da723483ce8ce3ddcc273c68443c30a229eb4
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-02-19 16:25:48 -0500

    binary search complete

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,25 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	const length = array.length;
+
+	if(length < 1){
+		return false
+	} else if( length === 1){
+		return  array[0] === target
+	} else {
+		const midpoint = Math.floor(length/2)
+		const left = array.slice(0, midpoint);
+		const right = array.slice(midpoint)
+		if(target === right[0]){
+			return true
+		} else if (target < right[0]){
+			return binarySearch(left,target)
+		} else {
+			return binarySearch(right,target)
+		}
+	}
+
 };
 
 /*
